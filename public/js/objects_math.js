0a75311df5100a7fbbb25195271a2d9a4e675115
jerald saenz <JandCo@jeralds-MacBook-Pro.local>
2015-10-09 10:38:49 -0500

    objects math

---
+++ public/js/objects_math.js
@@ -0,0 +1,26 @@
+"use strict";
+var circle = {
+	radius : 3,
+	getArea: function() {
+		var area = Math.PI * Math.pow(this.radius,2);
+		return area;
+	},
+	logInfo: function(round) {
+		var result = this.getArea();
+
+		if (round) {
+			console.log("Area of circle with radius " + this.radius + " is" + Math.round(result));
+		} else {
+			console.log("Area of circle with radius " + this.radius + " is" + result);
+		}
+		
+	}
+};
+
+circle.logInfo(false);
+circle.logInfo(true);
+
+circle.radius = 5
+
+circle.logInfo(false);
+circle.logInfo(true);
